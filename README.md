efd212b563bc2b196157a366deb9e96a6eb98a61
Colm Britton <colmjude@gmail.com>
2020-02-18 11:28:45 +0000

    Add renderer to create dev-cont-purp page

---
+++ README.md
@@ -0,0 +1,23 @@
+# Developer contribution purposes
+
+[![License](https://img.shields.io/github/license/mashape/apistatus.svg)](https://github.com/psd/openregister/blob/master/LICENSE)
+
+We recommend working in [virtual environment](http://docs.python-guide.org/en/latest/dev/virtualenvs/) before installing the python dependencies:
+
+    $ pipenv install
+
+The page can be created by running:
+
+    $ python render.py
+
+# Data
+
+The page is created using the [developer contribution purpose register](https://raw.githubusercontent.com/digital-land/alpha-data/master/mhclg-registers/developer-contribution-purpose.csv) which is maintained by MHCLG's Digital Land team.
+
+# Licence
+
+The software in this project is open source and covered by LICENSE file.
+
+Individual datasets copied into this repository may have specific copyright and licensing, otherwise all content and data in this repository is
+[© Crown copyright](http://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/copyright-and-re-use/crown-copyright/)
+and available under the terms of the [Open Government 3.0](https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/) licence.
---
+++ render.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import json
+import csv
+import jinja2
+
+import pandas as pd
+
+url = "https://raw.githubusercontent.com/digital-land/alpha-data/master/mhclg-registers/developer-contribution-purpose.csv"
+
+data = pd.read_csv(url,sep=",")
+
+# strip spaces introduced to values
+data_frame_trimmed = data.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+# strip spaces introduced to column headers
+data_frame_trimmed = data_frame_trimmed.rename(columns=lambda x: x.strip())
+
+json_data = json.loads(data_frame_trimmed.to_json(orient='records'))
+print(json_data)
+
+
+
+# set up paths to where the templates are
+def register_templates():
+    multi_loader = jinja2.ChoiceLoader([
+        jinja2.FileSystemLoader(searchpath="./templates"),
+        jinja2.PrefixLoader({
+            'govuk-jinja-components': jinja2.PackageLoader('govuk-jinja-components')
+        })
+    ])
+    return jinja2.Environment(loader=multi_loader)
+
+#from resource_generator.filters import pluralise, curie_org_url
+
+static_folder = "https://digital-land-design.herokuapp.com/static"
+
+# register filters with jinja context
+# def register_filters(env):
+#     env.filters["pluralise"] = pluralise
+#     env.filters["curie_url"] = curie_org_url
+
+# jinja setup
+env = register_templates()
+#register_filters(env)
+
+page_template = env.get_template("index.html")
+
+def render_page():
+    with open(f"developer-contribution-categories.html", "w") as f:
+        f.write(page_template.render(
+            static_folder=static_folder,
+            data=json_data)
+        )
+
+if __name__ == '__main__':
+    render_page()
